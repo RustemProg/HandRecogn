407d7c17a1efad992383d8e052adc625af17a8ee
Polos <85117866+RustemProg@users.noreply.github.com>
2023-05-28 18:31:44 +0500

    Add files via upload

--- man.py
+++ man.py
@@ -1,61 +1,61 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
-from cvzone.ClassificationModule import Classifier
 import numpy as np
 import math
- 
+import time
+
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
-classifier = Classifier("Model/keras_model.h5", "Model/labels.txt")
- 
+
 offset = 20
 imgSize = 300
  
-labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
- 
+folder = "Data\OK"
+counter = 0
+
 while True:
     success, img = cap.read()
-    imgOutput = img.copy()
+    if not success:
+        print("Failed to read frame from camera")
+        continue
+        
     hands, img = detector.findHands(img)
     if hands:
         hand = hands[0]
         x, y, w, h = hand['bbox']
- 
-        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+
         imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
- 
+        if imgCrop.size == 0 or imgCrop.shape[0] < 10 or imgCrop.shape[1] < 10:
+            print("Empty or invalid input image")
+            continue
+
         imgCropShape = imgCrop.shape
- 
         aspectRatio = h / w
- 
+
         if aspectRatio > 1:
             k = imgSize / h
             wCal = math.ceil(k * w)
             imgResize = cv2.resize(imgCrop, (wCal, imgSize))
             imgResizeShape = imgResize.shape
             wGap = math.ceil((imgSize - wCal) / 2)
+            imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
             imgWhite[:, wGap:wCal + wGap] = imgResize
-            prediction, index = classifier.getPrediction(imgWhite, draw=False)
-            print(prediction, index)
- 
+
         else:
             k = imgSize / w
             hCal = math.ceil(k * h)
             imgResize = cv2.resize(imgCrop, (imgSize, hCal))
             imgResizeShape = imgResize.shape
             hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
             imgWhite[hGap:hCal + hGap, :] = imgResize
-            prediction, index = classifier.getPrediction(imgWhite, draw=False)
- 
-        cv2.rectangle(imgOutput, (x - offset, y - offset-50),
-                      (x - offset+90, y - offset-50+50), (255, 0, 255), cv2.FILLED)
-        cv2.putText(imgOutput, labels[index], (x, y -26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
-        cv2.rectangle(imgOutput, (x-offset, y-offset),
-                      (x + w+offset, y + h+offset), (255, 0, 255), 4)
- 
- 
-        cv2.imshow("ImageCrop", imgCrop)
+
+        # cv2.imshow("ImageCrop", imgCrop)
         cv2.imshow("ImageWhite", imgWhite)
- 
-    cv2.imshow("Image", imgOutput)
-    cv2.waitKey(1)
+
+    cv2.imshow("Image", img)
+    key = cv2.waitKey(1)
+    if key == ord("a"):
+        counter += 1
+        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
+        print(counter)
