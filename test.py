56171ea5d25da5f61c9a5ed7983dc7814f75c419
Polos <85117866+RustemProg@users.noreply.github.com>
2023-05-28 18:37:17 +0500

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import math
  
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
-classifier = Classifier("Model/keras_model.h5", "Model/labels.txt")
+classifier = Classifier("keras_model.h5", "labels.txt")
  
 offset = 20
 imgSize = 300
